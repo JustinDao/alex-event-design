c6f49ac19d93f326c18916c8c3ce11bf8f5ad8f9
Justin Dao <tjustindao@gmail.com>
2020-07-21 23:09:40 -0700

    Typescript (#1)
    * Change to typescript
    
    * Add files
    
    * fix
    
    Co-authored-by: JustinDao <judao@microsoft.com>

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import "./App.css";
+import NavBar from "./Navbar";
+
+function App() {
+  return <NavBar />;
+}
+
+export default App;
---
+++ src/Navbar.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import Navbar from "react-bootstrap/Navbar";
+import Nav from "react-bootstrap/Nav";
+
+class NavBar extends React.Component {
+  render() {
+    return (
+      <Navbar bg="light" expand="lg">
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Navbar.Brand href="#home">Alex and Alfie</Navbar.Brand>
+          <Nav className="mr-auto">
+            <Nav.Link href="/about">About</Nav.Link>
+            <Nav.Link href="/services">Services</Nav.Link>
+            <Nav.Link href="/gallery">Gallery</Nav.Link>
+            <Nav.Link href="/contact">Contact</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
+    );
+  }
+}
+
+export default NavBar;
